2653d1a894ab7310073b6b69f7342202d900aafd
Carlos Ré <carlos.re@zharta.com>
2025-08-04 16:26:01 +0100

    add deployer

---
+++ scripts/_helpers/contracts.py
@@ -0,0 +1,174 @@
+import json
+from dataclasses import dataclass
+from hashlib import sha3_256
+
+from ape import project
+from ape.contracts.base import ContractContainer
+from ethpm_types.contract_type import ContractType
+from hexbytes import HexBytes
+from rich import print
+from rich.markup import escape
+
+from .basetypes import ContractConfig, DeploymentContext, abi_key
+from .transactions import check_owner, execute, execute_read
+
+ZERO_ADDRESS = "0x" + "00" * 20
+ZERO_BYTES32 = "0x" + "00" * 32
+
+
+def calculate_abi_key(filename: str) -> str:
+    with open(filename, "r") as f:
+        abi = json.load(f)
+    return abi_key(abi)
+
+
+class GenericContract(ContractConfig):
+    _address: str
+    _name: str
+
+    def __init__(self, *, key: str, address: str, abi_key: str, name: str, abi_file: str, version: str | None = None):
+        _abi_key = abi_key or calculate_abi_key(abi_file)
+        super().__init__(key, None, None, version=version, abi_key=_abi_key)
+        self._address = address
+        self._name = name
+
+    def address(self):
+        return self._address
+
+    def deployable(self, contract: DeploymentContext) -> bool:  # noqa: PLR6301, ARG002
+        return False
+
+    def __repr__(self):
+        return f"GenericContract[key={self.key}, address={self._address}]"
+
+
+@dataclass
+class P2PLendingErc20(ContractConfig):
+    def __init__(
+        self,
+        *,
+        key: str,
+        version: str | None = None,
+        abi_key: str,
+        payment_token_key: str,
+        collateral_token_key: str,
+        oracle_key: str,
+        kyc_validator_key: str | None = None,
+        protocol_upfront_fee: int,
+        protocol_settlement_fee: int,
+        protocol_wallet: str,
+        max_protocol_upfront_fee: int,
+        max_protocol_settlement_fee: int,
+        soft_liquidation_fee: int,
+        address: str | None = None,
+    ):
+        super().__init__(
+            key,
+            None,
+            project.P2PLendingErc20,
+            version=version,
+            abi_key=abi_key,
+            deployment_deps={payment_token_key, collateral_token_key, oracle_key, kyc_validator_key},
+            deployment_args=[
+                payment_token_key,
+                collateral_token_key,
+                oracle_key,
+                kyc_validator_key or ZERO_ADDRESS,
+                protocol_upfront_fee,
+                protocol_settlement_fee,
+                protocol_wallet,
+                max_protocol_upfront_fee,
+                max_protocol_settlement_fee,
+                soft_liquidation_fee,
+            ],
+        )
+        if address:
+            self.load_contract(address)
+
+
+@dataclass
+class CryptoPunks(ContractConfig):
+    def __init__(
+        self,
+        *,
+        key: str,
+        version: str | None = None,
+        abi_key: str,
+        address: str | None = None,
+    ):
+        super().__init__(
+            key,
+            None,
+            project.CryptoPunksMarketMock,
+            version=version,
+            abi_key=abi_key,
+            nft=True,
+        )
+        if address:
+            self.load_contract(address)
+
+
+@dataclass
+class Oracle(ContractConfig):
+    def __init__(
+        self,
+        *,
+        key: str,
+        version: str | None = None,
+        abi_key: str | None = None,
+        decimals: int | None = None,
+        rate: str | None = None,
+        address: str | None = None,
+    ):
+        super().__init__(
+            key,
+            None,
+            project.OracleMock,
+            version=version,
+            abi_key=abi_key,
+            token=True,
+            deployment_args=[decimals, int(rate) if rate else None],
+        )
+        if address:
+            self.load_contract(address)
+
+
+@dataclass
+class KYCValidator(ContractConfig):
+    def __init__(
+        self,
+        *,
+        key: str,
+        version: str | None = None,
+        abi_key: str | None = None,
+        validator: str,
+        address: str | None = None,
+    ):
+        super().__init__(
+            key,
+            None,
+            project.KYCValidator,
+            version=version,
+            abi_key=abi_key,
+            token=True,
+            deployment_args=[validator],
+        )
+        if address:
+            self.load_contract(address)
+
+
+@dataclass
+class ERC20External(ContractConfig):
+    def __init__(
+        self,
+        *,
+        key: str,
+        address: str | None = None,
+        abi_key: str | None = None,
+    ):
+        super().__init__(key, None, project.WETH9Mock, token=True, abi_key=abi_key)
+        if address:
+            self.load_contract(address)
+
+    def deployable(self, context: DeploymentContext) -> bool:  # noqa: PLR6301, ARG002
+        return False
---
+++ scripts/_helpers/deployment.py
@@ -0,0 +1,147 @@
+import json
+import logging
+import os
+import warnings
+from enum import Enum
+from pathlib import Path
+from typing import Any
+
+from ape import accounts
+
+from . import contracts as contracts_module
+from .basetypes import (
+    ContractConfig,
+    DeploymentContext,
+    Environment,
+)
+from .dependency import DependencyManager
+
+ENV = Environment[os.environ.get("ENV", "local")]
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+warnings.filterwarnings("ignore")
+
+
+class Context(Enum):
+    DEPLOYMENT = "deployment"
+    CONSOLE = "console"
+
+
+def load_contracts(env: Environment, chain: str) -> list[ContractConfig]:
+    config_file = Path.cwd() / "configs" / env.name / chain / "p2p-erc20.json"
+    with config_file.open(encoding="utf8") as f:
+        config = json.load(f)
+
+    return [
+        contracts_module.__dict__[c["contract"]](
+            key=f"{scope}.{name}", address=c.get("address"), abi_key=c.get("abi_key"), **c.get("properties", {})
+        )
+        for scope in ["common", "p2p"]
+        for name, c in config[scope].items()
+    ]
+
+
+def store_contracts(env: Environment, chain: str, contracts: list[ContractConfig]):
+    config_file = Path.cwd() / "configs" / env.name / chain / "p2p-erc20.json"
+    with config_file.open(encoding="utf8") as f:
+        config = json.load(f)
+
+    contracts_dict = {c.key: c for c in contracts}
+    for scope in ["common", "p2p"]:
+        for name, c in config[scope].items():
+            key = f"{scope}.{name}"
+            if key in contracts_dict:
+                c["address"] = contracts_dict[key].address()
+                if contracts_dict[key].abi_key:
+                    c["abi_key"] = contracts_dict[key].abi_key
+                if contracts_dict[key].version:
+                    c["version"] = contracts_dict[key].version
+            properties = c.get("properties", {})
+            addresses = c.get("properties_addresses", {})
+            for prop_key, prop_val in properties.items():
+                if prop_key.endswith("_key") and prop_val in contracts_dict:
+                    addresses[prop_key[:-4]] = contracts_dict[prop_val].address()
+            c["properties_addresses"] = addresses
+
+    with open(config_file, "w") as f:
+        f.write(json.dumps(config, indent=4, sort_keys=True))
+
+
+def load_tokens(env: Environment, chain: str) -> list[ContractConfig]:
+    config_file = Path.cwd() / "configs" / env.name / chain / "tokens.json"
+    with config_file.open(encoding="utf8") as f:
+        config = json.load(f)
+
+    return [
+        contracts_module.__dict__[c.get("contract_def", "ERC20External")](
+            key=f"common.{name}", address=c.get("address"), abi_key=c.get("abi_key")
+        )
+        for name, c in config.items()
+    ]
+
+
+def load_configs(env: Environment, chain: str) -> dict:
+    config_file = Path.cwd() / "configs" / env.name / chain / "p2p-erc20.json"
+    with config_file.open(encoding="utf8") as f:
+        config = json.load(f)
+
+    _configs = config.get("configs", {})
+    return {f"configs.{k}": v for k, v in _configs.items()}
+
+
+class DeploymentManager:
+    def __init__(self, env: Environment, chain: str, context: Context = Context.DEPLOYMENT):
+        self.env = env
+        self.chain = chain
+        match env:
+            case Environment.local:
+                self.owner = accounts.test_accounts[0]
+            case Environment.dev:
+                self.owner = accounts.load("devacc")
+            case Environment.int:
+                self.owner = accounts.load("intacc")
+            case Environment.prod:
+                self.owner = accounts.load("prodacc")
+        self.context = DeploymentContext(self._get_contracts(context), self.env, self.chain, self.owner, self._get_configs())
+
+    def _get_contracts(self, context: Context) -> dict[str, ContractConfig]:
+        contracts = load_contracts(self.env, self.chain)
+        tokens = load_tokens(self.env, self.chain)
+        all_contracts = contracts + tokens
+
+        # always deploy everything in local
+        if self.env == Environment.local and context == Context.DEPLOYMENT:
+            for contract in all_contracts:
+                contract.contract = None
+
+        return {c.key: c for c in all_contracts}
+
+    def _get_configs(self) -> dict[str, Any]:
+        return load_configs(self.env, self.chain)
+
+    def _save_state(self):
+        store_contracts(self.env, self.chain, list(self.context.contracts.values()))
+
+    def deploy(self, changes: set[str], *, dryrun=False, save_state=True):
+        self.owner.set_autosign(True) if self.env != Environment.local else None
+        self.context.dryrun = dryrun
+        dependency_manager = DependencyManager(self.context, changes)
+        contracts_to_deploy = dependency_manager.build_contract_deploy_set()
+        dependencies_tx = dependency_manager.build_transaction_set()
+
+        for contract in contracts_to_deploy:
+            if contract.deployable(self.context):
+                contract.deploy(self.context)
+
+        if save_state and not dryrun:
+            self._save_state()
+
+        for dependency_tx in dependencies_tx:
+            dependency_tx(self.context)
+
+        if save_state and not dryrun:
+            self._save_state()
+
+    def deploy_all(self, *, dryrun=False, save_state=True):
+        self.deploy(self.context.contract.keys(), dryrun=dryrun, save_state=save_state)
