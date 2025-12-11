495393ca0e261f354da882de83cd7800509d67ea
Carlos Ré <carlos.re@zharta.com>
2025-12-11 11:11:27 +0000

    add securitize vault with buy function to avoid proxy accreditation

--- tests/p2p_erc20_vaulted/integration/test_create_securitize.py
+++ tests/p2p_erc20_vaulted/integration/test_create_securitize.py
@@ -54,6 +54,16 @@ def oracle_acred_usd(oracle_contract_def, owner):
 
 
 @pytest.fixture
+def securitize_vault_contract_def():
+    return boa.load_partial("contracts/v2/P2PLendingV2VaultSecuritize.vy")
+
+
+@pytest.fixture
+def securitize_vault_impl(securitize_vault_contract_def):
+    return securitize_vault_contract_def.deploy()
+
+
+@pytest.fixture
 def p2p_usdc_acred(
     p2p_lending_erc20_contract_def,
     p2p_refinance,
@@ -63,7 +73,7 @@ def p2p_usdc_acred(
     oracle_acred_usd,
     kyc_validator_contract,
     owner,
-    vault_impl,
+    securitize_vault_impl,
     transfer_agent,
 ):
     return p2p_lending_erc20_contract_def.deploy(
@@ -81,7 +91,7 @@ def p2p_usdc_acred(
         0,
         p2p_refinance.address,
         p2p_liquidation.address,
-        vault_impl.address,
+        securitize_vault_impl.address,
         transfer_agent,
     )
 
@@ -115,16 +125,6 @@ def balancer(boa_env):
 def securitize_proxy(securitize_proxy_contract_def, p2p_usdc_acred, balancer, securitize_registry, securitize_owner, now):
     proxy = securitize_proxy_contract_def.deploy(p2p_usdc_acred.address, balancer.address)
     p2p_usdc_acred.set_proxy_authorization(proxy, True)
-    securitize_registry.updateInvestor(
-        "zharta_securitize_proxy",
-        "",
-        "PT",
-        [proxy.address],
-        [SEC_REG_ACCREDITED],
-        [SEC_REG_APPROVED],
-        [now + 86400 * 365],
-        sender=securitize_owner,
-    )
     return proxy
 
 
@@ -254,134 +254,6 @@ def test_create_loan(
     assert p2p_usdc_acred.commited_liquidity(liquidity_key) == principal
 
 
-@pytest.mark.skip
-def test_buy(
-    p2p_usdc_acred,
-    sec_borrower,
-    lender,
-    lender_key,
-    kyc_lender,
-    kyc_validator_contract,
-    kyc_validator_key,
-    usdc,
-    acred,
-    oracle_acred_usd,
-    securitize_registry,
-    securitize_swap,
-    securitize_proxy,
-):
-    principals = [70000000000, 49000000000, 34300000000, 24000000000, 17000000000]
-    collateral_amounts = [94000000, 66000000, 46000000, 32000000, 23000000]
-
-    # "offer_id": "0xb1b85a6840e529946c4fdacd8050337f538d28872fd19d08bfe99476fbe34345",
-    # "borrower": "0x81aF1E160c290E8Fff6381CCF67981f012Cf1009",
-    # "lender": "0xf1a9676B03Dd3B2066214D2aD8B4B59ED6642C53",
-    initial_borrower_collateral = collateral_amounts[0]
-    collateral_amount = sum(collateral_amounts)
-    collateral_to_buy = collateral_amount - initial_borrower_collateral
-    collateral_to_buy_value = securitize_swap.calculateStableCoinAmount(collateral_to_buy)
-    principal = sum(principals)
-
-    borrower = sec_borrower
-    # principal = 1000 * int(1e9)
-    # collateral_amount = 95 * int(1e6)
-    now = boa.eval("block.timestamp")
-    offer = Offer(
-        principal=principal,
-        payment_token=p2p_usdc_acred.payment_token(),
-        collateral_token=p2p_usdc_acred.collateral_token(),
-        duration=100,
-        min_collateral_amount=1,
-        available_liquidity=principal,
-        expiration=now + 100,
-        lender=lender,
-    )
-    signed_offer = sign_offer(offer, lender_key, p2p_usdc_acred.address)
-    kyc_borrower = sign_kyc(borrower, now, kyc_validator_key, kyc_validator_contract.address)
-
-    usdc.approve(securitize_swap.address, collateral_to_buy_value, sender=borrower)
-    securitize_swap.buy(collateral_to_buy, collateral_to_buy_value, sender=borrower)
-
-    acred.approve(p2p_usdc_acred.wallet_to_vault(borrower), collateral_amount, sender=borrower)
-    usdc.approve(p2p_usdc_acred.address, principal, sender=lender)
-
-    borrower_collateral_balance_before = acred.balanceOf(borrower)
-    borrower_balance_before = usdc.balanceOf(borrower)
-
-    now = boa.eval("block.timestamp")
-    boa.vm.py_evm.register_raw_precompile(
-        "0x0000000000000000000000000000000000011111", lambda computation: print("0x" + computation.msg.data.hex())
-    )
-
-    origination_fee = offer.origination_fee_bps * principal // BPS
-    lender_balance_before = usdc.balanceOf(lender)
-
-    loan_id = p2p_usdc_acred.create_loan(signed_offer, principal, collateral_amount, kyc_borrower, kyc_lender, sender=borrower)
-    event = get_last_event(p2p_usdc_acred, "LoanCreated")
-    initial_ltv = calc_ltv(principal, offer.min_collateral_amount, usdc, acred, oracle_acred_usd, oracle_reverse=False)
-
-    loan = Loan(
-        id=loan_id,
-        offer_id=compute_signed_offer_id(signed_offer),
-        offer_tracing_id=offer.tracing_id,
-        initial_amount=principal,
-        amount=principal,
-        apr=offer.apr,
-        payment_token=offer.payment_token,
-        collateral_token=offer.collateral_token,
-        maturity=now + offer.duration,
-        start_time=now,
-        accrual_start_time=now,
-        borrower=borrower,
-        lender=lender,
-        collateral_amount=collateral_amount,
-        min_collateral_amount=offer.min_collateral_amount,
-        origination_fee_amount=offer.origination_fee_bps * principal // BPS,
-        protocol_upfront_fee_amount=p2p_usdc_acred.protocol_upfront_fee(),
-        protocol_settlement_fee=p2p_usdc_acred.protocol_settlement_fee(),
-        partial_liquidation_fee=p2p_usdc_acred.partial_liquidation_fee(),
-        call_eligibility=offer.call_eligibility,
-        call_window=offer.call_window,
-        liquidation_ltv=offer.liquidation_ltv,
-        oracle_addr=p2p_usdc_acred.oracle_addr(),
-        initial_ltv=initial_ltv,
-        call_time=0,
-    )
-    assert compute_loan_hash(loan) == p2p_usdc_acred.loans(loan_id)
-
-    # event assertions
-    assert event.id == loan_id
-    assert event.amount == principal
-    assert event.apr == offer.apr
-    assert event.payment_token == offer.payment_token
-    assert event.maturity == now + offer.duration
-    assert event.start_time == now
-    assert event.borrower == borrower
-    assert event.lender == lender
-    assert event.collateral_token == offer.collateral_token
-    assert event.collateral_amount == collateral_amount
-    assert event.call_eligibility == offer.call_eligibility
-    assert event.call_window == offer.call_window
-    assert event.liquidation_ltv == offer.liquidation_ltv
-    assert event.oracle_addr == p2p_usdc_acred.oracle_addr()
-    assert event.initial_ltv == initial_ltv
-    assert event.origination_fee_amount == offer.origination_fee_bps * principal // BPS
-    assert event.protocol_upfront_fee_amount == p2p_usdc_acred.protocol_upfront_fee()
-    assert event.protocol_settlement_fee == p2p_usdc_acred.protocol_settlement_fee()
-    assert event.partial_liquidation_fee == p2p_usdc_acred.partial_liquidation_fee()
-    assert event.offer_id == compute_signed_offer_id(signed_offer)
-    assert event.offer_tracing_id == offer.tracing_id
-
-    assert acred.balanceOf(p2p_usdc_acred.wallet_to_vault(borrower)) == collateral_amount
-    assert acred.balanceOf(borrower) == borrower_collateral_balance_before - collateral_amount
-
-    assert usdc.balanceOf(borrower) == borrower_balance_before + principal - origination_fee
-    assert usdc.balanceOf(lender) == lender_balance_before - principal + origination_fee
-
-    liquidity_key = compute_liquidity_key(offer.lender, offer.tracing_id)
-    assert p2p_usdc_acred.commited_liquidity(liquidity_key) == principal
-
-
 def test_loop(
     p2p_usdc_acred,
     sec_borrower,
@@ -400,9 +272,6 @@ def test_loop(
     principals = [70000000000, 49000000000, 34300000000, 24000000000, 17000000000]
     collateral_amounts = [94000000, 66000000, 46000000, 32000000, 23000000]
 
-    # "offer_id": "0xb1b85a6840e529946c4fdacd8050337f538d28872fd19d08bfe99476fbe34345",
-    # "borrower": "0x81aF1E160c290E8Fff6381CCF67981f012Cf1009",
-    # "lender": "0xf1a9676B03Dd3B2066214D2aD8B4B59ED6642C53",
     initial_borrower_collateral = collateral_amounts[0]
     collateral_amount = sum(collateral_amounts)
     collateral_to_buy = collateral_amount - initial_borrower_collateral
